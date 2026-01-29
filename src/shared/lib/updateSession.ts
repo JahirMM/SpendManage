b35a38a4480c6331c20e728ebacdc06b85ba97e9
JahirMM <jahirmachuca45@gmail.com>
2026-01-29 00:18:54 -0300

    feat: implementar rutas protegidas con proxy

--- src/shared/lib/updateSession.ts
+++ src/shared/lib/updateSession.ts
@@ -33,7 +33,11 @@ export async function updateSession(request: NextRequest) {
 
   const user = data?.claims;
 
-  if (!user && !request.nextUrl.pathname.startsWith("/login")) {
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/login") &&
+    request.nextUrl.pathname !== "/"
+  ) {
     const url = request.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.redirect(url);
